395a9e0387b0fc56e71dd7d1517e683bd6e70e62
OMEN <OMEN@OMEN>
2019-11-01 14:14:57 +0800

    new create itemservice project

---
+++ sp02-itemservice/src/test/java/com/tedu/sp02/Sp02ItemserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp02;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp02ItemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
